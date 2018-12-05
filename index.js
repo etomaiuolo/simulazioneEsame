3ebccdd877777c7b53982faa105ea50026befd04
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-05 12:00:23 +0100

    Modificato

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const func = require("./function.js");
+const PORT = process.env.PORT || 3000
 
 
 app.get("/square", (req, res) => {
     res.send(func.string_square(req.query.string));
 });
+
+app.listen(PORT, () => console.log('App listening on port '+ PORT))
