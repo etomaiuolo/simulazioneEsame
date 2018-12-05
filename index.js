47b45fabc14b3fd1b614e5940c964ab57fac59f7
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-05 11:55:10 +0100

    Modificato

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const func = require("./function.js");
 
+
 app.get("/square", (req, res) => {
     res.send(func.string_square(req.query.string));
 });
